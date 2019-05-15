bfdfa1d16700e8493150084ef794549ea8d4d792
Dominik Dorfstetter <dorfstetter@posteo.de>
2019-05-15 16:30:32 +0200

    Update footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
    return (
       <footer>
-         <p>Created with ❤ by Dominik Dorfstetter, © 2019</p>
+         <p>Created with ❤ by Dominik Dorfstetter for adesso Austria GmbH, 2019</p>
       </footer>
    )
 }
