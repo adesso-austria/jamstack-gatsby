11cf8ddca29cfecbd61c333df34ee8ff9307f8dc
Dominik Dorfstetter <dorfstetter@posteo.de>
2019-06-13 19:09:30 +0200

    added install prompt

--- static/sw.js
+++ static/sw.js
@@ -1,12 +1,40 @@
 // Required stuff
 importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js'
-  )
-  
-  // The plugin will pass the files to cache here
-  workbox.precaching.precacheAndRoute([])
-  
-  // Another things
-  self.addEventListener('push', () => {
-    // ..
-  })
+  'https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js'
+)
+
+// The plugin will pass the files to cache here
+workbox.precaching.precacheAndRoute([])
+
+// Another things
+self.addEventListener('push', () => {
+  // ..
+})
+
+let deferredPrompt;
+
+self.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  btnAdd.style.display = 'block';
+});
+
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
