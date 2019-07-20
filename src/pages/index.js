a75d4079bbb1aed030a04ae9dec526670faadbab
Dominik Dorfstetter <dorfstetter@posteo.de>
2019-07-20 05:42:28 +0200

    updated core libraries // added spaces to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ const IndexPage = () => {
       <h1>WELCOME</h1>
       <hr/>
       <p>
-        GatsbyJS starter was developed in preperation for a talk hosted by
+        GatsbyJS starter was developed in preperation for a talk hosted by&nbsp;
         <a href="https://adesso.at" rel="noopener noreferrer" target="_blank">adesso Austria GmbH</a>.
         <br/><br/>
-        For a brief introduction to its features visit
+        For a brief introduction to its features visit&nbsp;
         <Link to="/about">About</Link>.
         <br/><br/>
-        <small>Do you want to contact
+        <small>Do you want to contact&nbsp;
           <Link to="/contact">
             us?</Link>
         </small>
